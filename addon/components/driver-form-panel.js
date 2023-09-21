8e69219f2ee0b47f3729551d8ba759963f6d70d2
charleyfleetbase <charley@fleetbase.io>
2023-09-21 11:56:43 +0700

    create driver component

---
+++ addon/components/driver-form-panel.js
@@ -0,0 +1,3 @@
+import Component from '@glimmer/component';
+
+export default class DriverFormPanelComponent extends Component {}
--- addon/routes.js
+++ addon/routes.js
@@ -30,6 +30,7 @@ export default buildRoutes(function () {
         });
         this.route('drivers', function () {
             this.route('index', { path: '/' });
+            this.route('new');
         });
         this.route('vehicles', function () {
             this.route('index', { path: '/' });
